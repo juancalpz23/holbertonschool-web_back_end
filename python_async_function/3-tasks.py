57c5367b7a91a04109f8b6056c478bbb59ed2a61
Juan C Lopez <juancalpz@yahoo.com>
2024-04-25 19:26:09 -0400

    Update: Fix added int n value

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -13,7 +13,7 @@ from typing import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task[float]:
+def task_wait_random(max_delay: int = 10) -> Task[float]:
     """
         Returns:
         asyncio.Task[float]: An asyncio.Task object
