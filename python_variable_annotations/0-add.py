f2a90550dbc0b93f8fc838a868ca79b8b80a1fbd
Juan C Lopez <juancalpz@yahoo.com>
2024-04-25 12:22:52 -0400

    Update - full script

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    add.py module
+    Write a type-annotated function add that takes a float
+    a and a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a:float, b: float) -> float:
+    """Adds two floating-point numbers"""
+    return a + b
