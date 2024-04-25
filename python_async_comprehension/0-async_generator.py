239c5eee2c6b8c5bda00af46cfbfb124a3d86fb9
Juan C Lopez <juancalpz@yahoo.com>
2024-04-25 19:55:11 -0400

    Update: Changed method to not use import

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,13 +7,12 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """
         Generate numbers and return float time random
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.randint(0, 10)
