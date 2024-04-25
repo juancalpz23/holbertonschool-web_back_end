4dfece505446feb9f2d0372cad4d949fdb05f2cb
Juan C Lopez <juancalpz@yahoo.com>
2024-04-25 19:28:50 -0400

    Update: Fix return

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -7,17 +7,17 @@
 """
 
 import asyncio
-from typing import Task
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int = 10) -> Task[float]:
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
         Returns:
         asyncio.Task[float]: An asyncio.Task object
         representing the execution of wait_random.
     """
-    loop = asyncio.get_event_loop()
-    return loop.create_task(wait_random(max_delay))
+    task = asyncio.create_task(wait_random(max_delay))
+
+    return task
