2539d6c390f8a617921e9249dbf9e85b22ee81e0
Juan C Lopez <juancalpz@yahoo.com>
2024-04-25 20:17:26 -0400

    Update: Testing modules

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""
-    Async Generator
-"""
+""" Async Generator """
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
-        Generate numbers and return float time random
+        Generate numbers
+
+        Args:
+            void
+
+        Return:
+            float time random
     """
     for _ in range(10):
         await asyncio.sleep(1)
