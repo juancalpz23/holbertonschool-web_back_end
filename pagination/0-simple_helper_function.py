e8f392ebb927df13880dedbdc2b3d62b7d28d91d
Juan C Lopez <juancalpz@yahoo.com>
2024-05-03 16:19:17 -0400

    CREATE: New file 0-simple_helper_function.py

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 
+    0-simple_helper_function.py Module
+    Write a function named index_range that takes
+    two integer arguments page and page_size.
+"""
+
+
+def index_range(page, page_size):
+    """
+        Returns a tuple of size two containing
+        a start index and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
