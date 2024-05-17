e10684c279a7cc4fb62ac8bffbad205efdfe73d7
Juan C Lopez <juancalpz@yahoo.com>
2024-05-17 15:57:04 -0400

    UPDATE: Fixing 6

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-export default function uploadPhoto(fileName) {
+function uploadPhoto(fileName) {
   return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
+
+export default uploadPhoto('test.jpg');
