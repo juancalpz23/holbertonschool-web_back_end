f8791512ec645d8d48c7ff0c3e3c63f44c3a2bb3
Juan C Lopez <juancalpz@yahoo.com>
2024-05-17 13:05:12 -0400

    UPDATE: Eslint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-      // Simulating an async operation, you can resolve or reject based on some conditions
-      resolve('Success');
+  return new Promise((resolve) => {
+    // Simulating an async operation, you can resolve or reject based on some conditions
+    resolve('Success');
   });
 }
