2da04d6a04d4764ba236c60deea1c1e6e1cae414
Juan C Lopez <juancalpz@yahoo.com>
2024-09-19 15:51:13 -0400

    Update: pep8

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -14,12 +14,12 @@ class BasicCache(BaseCaching):
         BasicCache inherits from BaseCaching and is a basic caching system
         It does not have any limit on the size of the cache
     """
-    
+
     def put(self, key, item):
 
-        """ 
+        """
             Add an item in the cache.
-            If key or item is None, do nothing.
+            If key or item is None, do nothing
         """
 
         if key is not None and item is not None:
@@ -29,6 +29,6 @@ class BasicCache(BaseCaching):
 
         """
             Get an item by key.
-            If key is None or doesn't exist in the cache, return None.
+            If key is None or doesn't exist in the cache, return None
         """
         return self.cache_data.get(key, None)
