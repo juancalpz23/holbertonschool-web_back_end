6c76f0a761caf5b671a13ed276707331424a1a6f
Juan C Lopez <juancalpz@yahoo.com>
2024-09-30 17:03:33 -0400

    Task 0: Regex-ing

---
+++ personal_data/README.md
@@ -0,0 +1,1 @@
+Holberton Personal Data
---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Defines filter_datum function to obfuscate fields in a log message.
+"""
+
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates specific fields in a log message by replacing their values with redaction.
+    """
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}", f"\\1={redaction}{separator}", message)
