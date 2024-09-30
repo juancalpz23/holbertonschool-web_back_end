187ba3f97b27959a45022903dc4d116bdfabafb3
Juan C Lopez <juancalpz@yahoo.com>
2024-09-30 17:07:46 -0400

    Update: Betty

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,4 +10,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     Obfuscates specific fields in a log message by replacing their values with redaction.
     """
+
     return re.sub(f"({'|'.join(fields)})=.*?{separator}", f"\\1={redaction}{separator}", message)
