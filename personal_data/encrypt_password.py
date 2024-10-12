19b6c8335e411c7e6ba2e1fb0c457ef3bbffb1d3
Juan C Lopez <juancalpz@yahoo.com>
2024-10-11 20:24:32 -0400

    Task 6: Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -18,3 +18,11 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password.
+    """
+    # Use bcrypt to check if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode(), hashed_password)
