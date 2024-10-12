4d79c2a2d10528e8bbd55f0dbe0cb0fe3c08405c
Juan C Lopez <juancalpz@yahoo.com>
2024-10-11 20:45:45 -0400

    Task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,38 +2,14 @@
 """
 Defines filter_datum function to obfuscate fields in a log message.
 """
-
-import logging
+import re
 from typing import List
-from re import sub
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Hide specific fields in a log message by replacing them with redactions.
     """
-    return sub(f"({'|'.join(fields)})=.*?{separator}",
-               f"\\1={redaction}{separator}", message)
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """ Initialize the formatter with fields to be redacted """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        Format the log record and filter sensitive information.
-        """
-        original_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            original_message, self.SEPARATOR)
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}",
+                  f"\\1={redaction}{separator}", message)
