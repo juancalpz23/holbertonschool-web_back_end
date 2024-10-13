cdc296887c7fa55314d8f56e7bd25c0cac56699a
Juan C Lopez <juancalpz@yahoo.com>
2024-10-13 09:46:03 -0400

    Task 10: Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,9 @@ BasicAuth class that inherits from Auth
 """
 
 from api.v1.auth.auth import Auth
-import base64  # To handle Base64 decoding
+from models.user import User  # Import User model
+from typing import TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -58,3 +60,39 @@ class BasicAuth(Auth):
         # Split the string into email and password
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from the decoded Base64 string.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on the provided email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Search for user by email
+        users = User.search({'email': user_email})
+        if not users or len(users) == 0:
+            return None  # No user found
+
+        user = users[0]  # Assume the first match is the user we're looking for
+
+        # Validate the password
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
