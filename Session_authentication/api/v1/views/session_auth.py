5896c0c12fc810a947fb180b92a0a76985b702ec
Juan C Lopez <juancalpz@yahoo.com>
2024-10-13 12:23:37 -0400

    Task 8: Logout

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -44,3 +44,18 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return
+        - empty JSON dictionary if logged out successfully
+    """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
