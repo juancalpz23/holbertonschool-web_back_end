5084a5123900f51ec9cb8ad0a501f5845fb26637
Juan C Lopez <juancalpz@yahoo.com>
2024-10-13 07:42:39 -0400

    Task 2: Error handler: Forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,7 +8,8 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
       - the status of the API
     """
@@ -16,7 +18,8 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """
+    GET /api/v1/stats
     Return:
       - the number of each objects
     """
@@ -28,8 +31,19 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_endpoint():
-    """ GET /api/v1/unauthorized
+    """
+    GET /api/v1/unauthorized
     Raises:
       - 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint():
+    """
+    GET /api/v1/forbidden
+    Raises:
+      - 403 error
+    """
+    abort(403)
