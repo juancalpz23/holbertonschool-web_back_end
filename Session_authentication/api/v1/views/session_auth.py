3e0de296ccf59eedcd27f3e1ebbe61cdb0a29b84
Juan C Lopez <juancalpz@yahoo.com>
2024-10-13 12:07:55 -0400

    Update: Fix

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -6,10 +6,8 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models.user import User
 from os import getenv
-from api.v1.app import auth  # Import here to avoid circular import
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 @app_views.route('/auth_session/login/',
                  methods=['POST'], strict_slashes=False)
 def login() -> str:
