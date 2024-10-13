432cde193119d2f9c05f2c2009fae870a59642dc
Juan C Lopez <juancalpz@yahoo.com>
2024-10-13 09:15:41 -0400

    Task 6: Basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-
-
 """
 Route module for the API
 """
-
-
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
-import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -20,32 +15,44 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # Import and initialize authentication class based on AUTH_TYPE
 auth = None
 auth_type = getenv("AUTH_TYPE")
-if auth_type == "auth":
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """
+    Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
+    """
+    Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler """
+    """
+    Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request_handler():
-    """ Handler for filtering each request before it is processed. """
+    """
+    Handler for filtering each request before it is processed.
+    """
     if auth is None:
         return  # Do nothing if no authentication instance is available
 
@@ -70,7 +77,6 @@ def before_request_handler():
 
 
 if __name__ == "__main__":
-
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    Basic authentication class that extends Auth
+    """
+    pass
