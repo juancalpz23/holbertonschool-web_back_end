9917db07d242442c15a94bafabe1b80abacb32ce
Juan C Lopez <juancalpz@yahoo.com>
2024-10-17 16:12:54 -0400

    Task 4: Hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Authentication module for password hashing.
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+    """
+    salt = bcrypt.gensalt()  # Generate a salt
+    return bcrypt.hashpw(password.encode(), salt)  # Hash and return the password
