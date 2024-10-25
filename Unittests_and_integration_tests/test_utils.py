01b0dca84f65bfe9343d6f1d1462b8441ec01c99
Juan C Lopez <juancalpz@yahoo.com>
2024-10-25 04:31:04 -0400

    Update: New text exceptions

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,27 +10,25 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Test suite for access_nested_map function.
+    Test access_nested_map with various nested dictionaries and paths.
     """
 
-    @expand([
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Test access_nested_map with various nested dictionaries and paths.
-
-        Args:
-            nested_map (dict): The nested dictionary to access.
-            path (tuple): A tuple representing the path of keys.
-            expected (Any): The expected value at the end of the path.
-
-        Asserts:
-            The result of access_nested_map matches the expected value.
-        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 
 if __name__ == "__main__":
