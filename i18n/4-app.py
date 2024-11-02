15f47f42e4fc7a770ae234a9af16a6009f0d9e28
Juan C Lopez <juancalpz@yahoo.com>
2024-11-02 14:58:09 -0400

    Update: Pycodestyle

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -6,15 +6,18 @@ from flask_babel import Babel, _
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 
 babel = Babel()
+
 
 def get_locale():
     """Determine the best match with supported languages."""
@@ -25,12 +28,15 @@ def get_locale():
     # Fallback to the best match with accept languages
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world():
     """Render the template with translatable strings"""
     return render_template('4-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
