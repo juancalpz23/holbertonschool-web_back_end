615649f347c0f474ccc82134cfa0b87d62b86b9c
Juan C Lopez <juancalpz@yahoo.com>
2024-11-02 13:54:56 -0400

    Pycodestyle

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -6,20 +6,24 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world():
     """Return the template"""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
