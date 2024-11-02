3a25efab1dbf9edcab193141d71ef582935edc30
Juan C Lopez <juancalpz@yahoo.com>
2024-11-02 13:10:29 -0400

    Documentation

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+"""Flask app"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    """Return the template"""
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
