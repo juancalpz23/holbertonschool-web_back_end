01dfd55a3efeeead58a1cf970ba4e088254dc65f
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 14:11:20 -0400

    Update: Fix

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -2,19 +2,14 @@
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(5, 2);
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
 END $$
 
 DELIMITER ;
