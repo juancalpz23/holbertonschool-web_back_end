a0aaacd3b5b895ff33a2df062cc1880618ab728e
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 13:51:08 -0400

    Update: Fix

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,4 @@
 -- Script to rank band origins by number of non-unique fans
 
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin
 ORDER BY nb_fans DESC;
