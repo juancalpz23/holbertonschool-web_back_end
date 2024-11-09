99e480fc06dedc9224ad37fc7079b5492dc1abbf
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 13:56:37 -0400

    Update: Test

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Script to list Glam rock bands ranked by longevity
 
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%' ;
