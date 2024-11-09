c5ed19afdf11df9a08a5bc5f471e499c0b5d9765
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 14:04:45 -0400

    Update: Delimiter

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,14 @@
 -- Script to create a trigger that resets valid_email when the email is changed
 -- This trigger updates valid_email to 0 whenever a user's email address is modified.
 
-CREATE TRIGGER reset_valid_email_before_update
-BEFORE UPDATE ON users
+DELIMITER $$
+
+CREATE TRIGGER new_email BEFORE UPDATE ON users
 FOR EACH ROW
-    IF OLD.email <> NEW.email THEN
-        SET NEW.valid_email = 0;
+BEGIN
+    IF OLD.email != NEW.email THEN
+	SET NEW.valid_email = 0;
     END IF;
+END$$
+
+DELIMITER ;
