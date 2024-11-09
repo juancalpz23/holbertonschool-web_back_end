5a49fd26a4ff612c6eabe9cedec182df68852f91
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 13:59:40 -0400

    Update: Fix

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,14 +1,7 @@
 -- Script to create a trigger for updating item quantity after a new order
 
-DELIMITER //
-
-CREATE TRIGGER decrease_item_quantity_after_order
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
-END //
-
-DELIMITER ;
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET
+quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
